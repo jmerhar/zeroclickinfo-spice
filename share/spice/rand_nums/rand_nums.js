1d05f210cb740a5fcbc1a68da1dbd14733096ed5
Jag Talon <jagtalon@duckduckgo.com>
2014-04-22 19:09:26 -0400

    rand_nums.js: started to port rand_nums to BTTF.

--- share/spice/rand_nums/rand_nums.js
+++ share/spice/rand_nums/rand_nums.js
@@ -1,38 +1,44 @@
-function ddg_spice_rand_nums(api_result) {
-    "use strict";
+(function(env) {
+    env.ddg_spice_rand_nums = function(api_result) {
+	
+    };
+}(this));
 
-    var min;
-    var max;
+// function ddg_spice_rand_nums(api_result) {
+//     "use strict";
 
-    // Get the original query.
-    // We're going to pass this to the header.
-    var query = "";
-    $("script").each(function() {
-        var matched, result;
-        matched = $(this).attr("src");
-        if(matched) {
-            result = matched.match(/\/js\/spice\/rand_nums\/([^\/]+)\/(.+)/);
-            if(result) {
-                min = result[1];
-                max = result[2];
-            }
-        }
-    });
+//     var min;
+//     var max;
 
-    min = min || 0;
-    max = max || 100;
+//     // Get the original query.
+//     // We're going to pass this to the header.
+//     var query = "";
+//     $("script").each(function() {
+//         var matched, result;
+//         matched = $(this).attr("src");
+//         if(matched) {
+//             result = matched.match(/\/js\/spice\/rand_nums\/([^\/]+)\/(.+)/);
+//             if(result) {
+//                 min = result[1];
+//                 max = result[2];
+//             }
+//         }
+//     });
 
-    Spice.add({
-		id: "rand_num",
-        data             : api_result,
-        meta {
-            sourceUrl       : 'http://www.random.org/integers/?num=100&min='
-                                + min + '&max=' + max + '&col=5&base=10&format=html&rnd=new',
-            sourceName      : 'Random.org',
-        },
-        templates: {
-            item: Spice.rand_nums.rand_nums,
-            detail: Spice.rand_nums.rand_nums
-        },
-    });
-}
+//     min = min || 0;
+//     max = max || 100;
+
+//     Spice.add({
+// 		id: "rand_num",
+//         data             : api_result,
+//         meta {
+//             sourceUrl       : 'http://www.random.org/integers/?num=100&min='
+//                                 + min + '&max=' + max + '&col=5&base=10&format=html&rnd=new',
+//             sourceName      : 'Random.org',
+//         },
+//         templates: {
+//             item: Spice.rand_nums.rand_nums,
+//             detail: Spice.rand_nums.rand_nums
+//         },
+//     });
+// }
