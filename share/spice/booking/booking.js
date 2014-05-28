6010cc57426144c7d159d021a3169a2f680cb503
Dax the Duck <dax@duckduckhack.(none)>
2014-05-28 06:28:41 -0400

    Initial JS code for Booking.com instant answer

---
+++ share/spice/booking/booking.js
@@ -0,0 +1,26 @@
+(function (env) {
+    "use strict";
+    env.ddg_spice_booking = function(api_result){
+
+        if (api_result.error) {
+            return Spice.failed('booking');
+        }
+
+        Spice.add({
+            id: "booking",
+            name: "Booking",
+            data: api_result,
+            meta: {
+                sourceName: "booking.com",
+                sourceUrl: 'http://www.booking.com/hotel/' + api_result.cc1 + '/' + api_result.pagename + '.html'
+            },
+            templates: {
+                group: 'base',
+                options: {
+                    content: Spice.booking.content,
+                    moreAt: true
+                }
+            }
+        });
+    };
+}(this));
