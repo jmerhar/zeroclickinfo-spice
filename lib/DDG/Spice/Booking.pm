b9510e458c97cf6106a6490ef3d3a52bc08d813b
Dax the Duck <dax@duckduckhack.(none)>
2014-07-31 10:55:40 -0400

    changed to production url for Booking.com

--- lib/DDG/Spice/Booking.pm
+++ lib/DDG/Spice/Booking.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 
 triggers startend =>'hotels';
 
-spice to =>'http://jmerhar-app.dev.booking.com/duckduckhotels?city=$1&callback=ddg_spice_booking';
+spice to =>'http://www.booking.com/duckduckhotels?city=$1&callback=ddg_spice_booking';
 
 #spice wrap_joson_callback => 1;
 
